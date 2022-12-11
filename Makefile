37a063f73830cfd828da0605861925c2e75e5363
Arturgo <arturgo@hotmail.fr>
2022-12-11 22:20:15 +0100

    Add optimal algorithm

---
+++ Makefile
@@ -0,0 +1,11 @@
+# Copyright 2022, Gurobi Optimization, LLC
+
+PLATFORM = linux64
+INC      = /home/arthur/ENS/PACE22/gurobi951/linux64/include/
+CPP      = g++
+CARGS    = -m64 -g
+CPPLIB   = -L/home/arthur/ENS/PACE22/gurobi951/linux64/lib/ -lgurobi_c++ -lgurobi95
+
+
+main_gurobi:
+	$(CPP) $(CARGS) -o optim optim.cpp -std=c++11 $< -I$(INC) $(CPPLIB) -lm
---
+++ optim.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include "gurobi_c++.h"
+using namespace std;
+
+struct Transaction {
+	int sender, receiver;
+	double amount;
+};
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	GRBEnv env;
+	env.set(GRB_IntParam_LogToConsole, 0);
+	GRBModel model = GRBModel(env);
+	
+	int nbBanks, nbTransactions;
+	cin >> nbBanks >> nbTransactions;
+	
+	vector<GRBVar> vars(nbTransactions);
+	vector<GRBLinExpr> constrs(nbBanks);
+	
+	for(int iBank = 0;iBank < nbBanks;iBank++) {
+		double liq;
+		cin >> liq;
+		constrs[iBank] += liq;
+	}
+	
+	double upperThroughput = 0.;
+	
+	for(int iTransaction = 0;iTransaction < nbTransactions;iTransaction++) {
+		int sender, receiver;
+		double amount;
+		cin >> sender >> receiver >> amount;
+		
+		upperThroughput += amount;
+		
+		vars[iTransaction] = model.addVar(
+			0.0, 1.0, -amount, GRB_BINARY, "x_" + to_string(iTransaction)
+		);
+		
+		constrs[sender] += -amount * vars[iTransaction];
+		constrs[receiver] += amount * vars[iTransaction];
+	}
+	
+	for(int iBank = 0;iBank < nbBanks;iBank++) {
+		model.addConstr(0. <= constrs[iBank]);
+	}
+	
+	model.optimize();
+	
+	cout << -model.get(GRB_DoubleAttr_ObjVal) << " " << upperThroughput << endl;
+	
+	return 0;
+}
